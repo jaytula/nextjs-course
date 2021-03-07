032295a85d4a7e0531dcf1a173ac1cf8fa98b75b
Jay T <jaytula@gmail.com>
2021-03-07 13:28:49 -0800

    Remove Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Hello Next World!</h1>
     </div>
   )
