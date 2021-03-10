df4bdd7ec616deb70cfa21bf4ab23d8352129968
Jay T <jaytula@gmail.com>
2021-03-09 19:28:36 -0800

    Lesson 3.59: Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link replace href="/clients">Clients List</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
     </div>
   );
 }
