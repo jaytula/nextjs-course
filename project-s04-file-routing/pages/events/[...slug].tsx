04f7471c0832782dab5ddf987c7a3079fb8affe5
Jay T <jaytula@gmail.com>
2021-03-15 14:20:46 -0700

    Lesson 4.79: Extracting Data on the Catch-All Page

--- project-s04-file-routing/pages/events/[...slug].tsx
+++ project-s04-file-routing/pages/events/[...slug].tsx
@@ -1,13 +1,39 @@
 import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import { getFilteredEvents } from "../../dummy-data";
 
 const FilteredEventsPage = () => {
   const router = useRouter();
-  const { query } = router;
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading</p>;
+  }
+
+  const year = +filterData[0];
+  const month = +filterData[1];
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return <p className="center">Invalid filter. Please adjust your values.</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year, month });
+
+  if(!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter.</p>
+  }
 
   return (
     <div>
       <h1>Filtered Events Page</h1>
-      <p>Query: {JSON.stringify(query)}</p>
+      <EventList items={filteredEvents} />
     </div>
   );
 };
