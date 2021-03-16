a56a0784f571d2213167823a74dfcc9331f65a38
Jay T <jaytula@gmail.com>
2021-03-16 14:49:25 -0700

    Lesson 5.88: Adding 'getStaticProps' to pages

--- project-s05-prerender-data-fetching/pages/index.js
+++ project-s05-prerender-data-fetching/pages/index.js
@@ -1,11 +1,20 @@
+import data from '../data/dummy-backend.json';
+
 function HomePage(props) {
+  const { products } = props
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map(product => {
+        return <li key={product.id}>{product.title}</li>
+      })}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: data
+  }
 }
 
 export default HomePage;
