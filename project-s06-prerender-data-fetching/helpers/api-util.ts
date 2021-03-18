28ae04d0ca61caec64aeb430ba7d45d704e0e75a
Jay T <jaytula@gmail.com>
2021-03-18 14:46:42 -0700

    Lesson 6.114: Adding Static Site Generation (SSG) On the Home Page

---
+++ project-s06-prerender-data-fetching/helpers/api-util.ts
@@ -0,0 +1,25 @@
+const FIREBASE_BACKEND = process.env.NEXT_PUBLIC_FIREBASE_BACKEND as string;
+
+export interface Event {
+  id: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  image: string;
+  isFeatured: boolean;
+}
+
+export const getAllEvents: () => Promise<Event[]> = async () => {
+  return fetch(`${FIREBASE_BACKEND}events.json`)
+    .then((res) => res.json())
+    .then((data: { [key: string]: Event }) => {
+      const transformedData = Object.entries(data).map(([key, value]) => value);
+      return transformedData;
+    });
+};
+
+export const getFeaturedEvents: () => Promise<Event[]> = async () => {
+  const allEvents = await getAllEvents();
+  return allEvents.filter(event => event.isFeatured);
+}
---
+++ project-s06-prerender-data-fetching/pages/index.tsx
@@ -0,0 +1,22 @@
+import EventList from "../components/events/event-list";
+import {getFeaturedEvents, Event} from '../helpers/api-util';
+import { GetStaticProps } from "next";
+
+const HomePage: React.FC<{events: Event[]}> = ({events}) => {
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const events = await getFeaturedEvents();
+  return {
+    props: {
+      events
+    }
+  }
+};
+
+export default HomePage;
