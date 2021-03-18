8994445f20c706760deea4cd9a14506de12af37e
Jay T <jaytula@gmail.com>
2021-03-18 09:02:12 -0700

    Lesson 5.108: Using the "useSWR" NextJS Hook

--- project-s05-prerender-data-fetching/pages/last-sales.tsx
+++ project-s05-prerender-data-fetching/pages/last-sales.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 interface Data {
   [key: string]: Sale;
@@ -12,29 +13,50 @@ interface Sale {
 const FIREBASE_BACKEND = process.env.NEXT_PUBLIC_FIREBASE_BACKEND;
 const LastSalesPage = () => {
   const [sales, setSales] = useState<Sale[] | undefined>();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR<Sale[] | undefined>(`${FIREBASE_BACKEND}sales.json`);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(`${FIREBASE_BACKEND}sales.json`)
-      .then((res) => res.json())
-      .then((data: Data) => {
-        const transformedData = Object.entries(data).map(([key, value]) => ({
-          id: key,
-          username: value.username,
-          volume: value.volume,
-        }));
+    if (data) {
+      const transformedData = Object.entries(data).map(([key, value]) => ({
+        id: key,
+        username: value.username,
+        volume: value.volume,
+      }));
 
-        setSales(transformedData);
-        setIsLoading(false);
-      });
-  }, []);
+      setSales(transformedData);
+    }
+  }, [data]);
 
-  if (isLoading) return <p>Loading...</p>;
-
-  if(!sales) {
-    return <p>No data yet...</p>
+  if (error) {
+    return <p>Failed to load.</p>;
   }
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(`${FIREBASE_BACKEND}sales.json`)
+  //     .then((res) => res.json())
+  //     .then((data: Data) => {
+  //       const transformedData = Object.entries(data).map(([key, value]) => ({
+  //         id: key,
+  //         username: value.username,
+  //         volume: value.volume,
+  //       }));
+
+  //       setSales(transformedData);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  // if (isLoading) return <p>Loading...</p>;
+
+  // if(!sales) {
+  //   return <p>No data yet...</p>
+  // }
 
   console.log({ FIREBASE_BACKEND });
   return (
