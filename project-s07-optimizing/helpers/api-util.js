063135926fa2a77e3c79d99a1aa2cf2a4b30a75a
Jay T <jaytula@gmail.com>
2021-03-20 02:13:41 -0700

    Fix firebase backend url

--- project-s07-optimizing/helpers/api-util.js
+++ project-s07-optimizing/helpers/api-util.js
@@ -1,5 +1,6 @@
+const FIREBASE_BACKEND = process.env.NEXT_PUBLIC_FIREBASE_BACKEND;
 export async function getAllEvents() {
-  const response = await fetch('https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json');
+  const response = await fetch(`${FIREBASE_BACKEND}events.json`);
   const data = await response.json();
 
   const events = [];
