1ebf88e0fa4935342ef9f181c2a1f012e2e2cc4d
Jay T <jaytula@gmail.com>
2021-03-20 02:04:40 -0700

    Lesson 7.124: Configuring the "head" Content

--- project-s07-optimizing/pages/index.js
+++ project-s07-optimizing/pages/index.js
@@ -1,9 +1,16 @@
-import { getFeaturedEvents } from '../helpers/api-util';
-import EventList from '../components/events/event-list';
+import Head from "next/head";
+
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Find stuff to help you evolve" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
@@ -14,10 +21,10 @@ export async function getStaticProps() {
 
   return {
     props: {
-      events: featuredEvents
+      events: featuredEvents,
     },
-    revalidate: 1800
-  }
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
