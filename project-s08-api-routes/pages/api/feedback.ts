7d66f2f989cffd50f2d0ec749ba374d0097e7d95
Jay T <jaytula@gmail.com>
2021-03-23 15:15:12 -0700

    Lesson 8.140. Parsing the Incoming Request & Executing Server-side Code

--- project-s08-api-routes/pages/api/feedback.ts
+++ project-s08-api-routes/pages/api/feedback.ts
@@ -1,7 +1,31 @@
 import { NextApiHandler } from "next";
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
 
-const handler: NextApiHandler = (req, res) => {
-  res.status(200).json({ message: "This works!" });
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const handler: NextApiHandler = async (req, res) => {
+  if(req.method === 'POST') {
+    const {email, text} = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    }
+
+    // store that in a database or in a file
+    const FEEDBACK_FILE = path.join(process.cwd(), 'data', 'feedback.json')
+    const fileContents = await readFile(FEEDBACK_FILE);
+    const feedbacks = fileContents.toString() ? JSON.parse(fileContents.toString()) : [];
+    feedbacks.push(newFeedback);
+    await writeFile(FEEDBACK_FILE, JSON.stringify(feedbacks, null, 2))
+    res.status(201).json({message: 'Success!', feedback: newFeedback})
+  } else {
+    res.status(200).json({ message: "This works!" });
+  }
 };
 
 export default handler;
--- project-s08-api-routes/pages/index.js
+++ project-s08-api-routes/pages/index.js
@@ -7,9 +7,18 @@ function HomePage() {
   const submitFormHandler = (event) => {
     event.preventDefault();
 
-    const enteredEmail = emailInputRef.current.value
+    const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
-  }
+
+    fetch("/api/feedback", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: enteredEmail,
+        text: enteredFeedback,
+      }),
+    });
+  };
   return (
     <div>
       <h1>The Home Page</h1>
@@ -20,7 +29,7 @@ function HomePage() {
         </div>
         <div>
           <label htmlFor="feedback">Your Email Address</label>
-          <textarea id="feedback" rows='5' ref={feedbackInputRef}></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
