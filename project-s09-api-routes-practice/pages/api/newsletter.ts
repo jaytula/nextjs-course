6011a45d8397ac7ff86f394e1afe4888d2390927
Jay T <jaytula@gmail.com>
2021-03-26 23:44:33 -0700

    Lesson 9.158: Adding Error Handling

--- project-s09-api-routes-practice/pages/api/newsletter.ts
+++ project-s09-api-routes-practice/pages/api/newsletter.ts
@@ -1,6 +1,22 @@
 import { NextApiHandler } from "next";
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  return client;
+}
+
+async function insertDocument(
+  client: MongoClient,
+  document: { email: string }
+) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 const handler: NextApiHandler = async (req, res) => {
   if (req.method !== "POST") {
     res.status(405).end();
@@ -14,17 +30,24 @@ const handler: NextApiHandler = async (req, res) => {
     return;
   }
 
-  const client = await MongoClient.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  const db = client.db();
+  let client: MongoClient;
 
-  await db.collection("newsletter").insertOne({
-    email,
-  });
+  try {
+    client = await connectDatabase();
+  } catch (err) {
+    res.status(500).json({ message: 'Connecting to the database failed' });
+    return;
+  }
 
-  client.close();
+  try {
+    await insertDocument(client, {
+      email,
+    });
+    client.close();
+  } catch (err) {
+    res.status(500).json({ message: 'Inserting data failed' });
+    return;
+  } 
 
   res.status(201).json({
     message: "Signed up!",
