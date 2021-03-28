7074bbdc89d466e99bdcb82fb1892354df7b59d2
Jay T <jaytula@gmail.com>
2021-03-28 10:50:56 -0700

    Lesson 10.167: Using Context Data in Components

--- project-s09-api-routes-practice/components/layout/layout.tsx
+++ project-s09-api-routes-practice/components/layout/layout.tsx
@@ -1,12 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from "react";
+import NotificationContext, {
+  NotificationIface,
+} from "../../store/notification-context";
+import Notification from "../ui/notification";
 
-import MainHeader from './main-header';
+import MainHeader from "./main-header";
 
 function Layout(props) {
+  const { notification } = useContext<NotificationIface>(NotificationContext);
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {notification ? (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      ) : null}
     </Fragment>
   );
 }
