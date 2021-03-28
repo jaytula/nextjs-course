8f7d608a612bbe3095c078c8f80b7957ec30ba06
Jay T <jaytula@gmail.com>
2021-03-27 21:31:30 -0700

    Lesson 10.166: Adding Context State

--- project-s09-api-routes-practice/store/notification-context.tsx
+++ project-s09-api-routes-practice/store/notification-context.tsx
@@ -1,12 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export interface Notification {
-  notification: null | {
-    title: string;
-    message: string;
-    status: string;
-  };
-  showNotification: () => void;
+type NotificationData = {
+  title: string;
+  message: string;
+  status: string;
+};
+
+export interface NotificationIface {
+  notification: null | NotificationData;
+  showNotification: (notification: NotificationData) => void;
   hideNotification: () => void;
 }
 
@@ -16,12 +18,32 @@ const defaultContext = {
   hideNotification: function () {},
 };
 
-const NotificationContext = createContext<Notification>(defaultContext);
+const NotificationContext = createContext<NotificationIface>(defaultContext);
 
-export const NotificationContextProvider = ({ children }) => (
-  <NotificationContext.Provider value={defaultContext}>
-    {children}
-  </NotificationContext.Provider>
-);
+export const NotificationContextProvider = ({ children }) => {
+  const [
+    activeNotification,
+    setActiveNotification,
+  ] = useState<null | NotificationData>(null);
+
+  const showNotificationHandler = (notificationData: NotificationData) => {
+    setActiveNotification(notificationData);
+  };
+  const hideNotificationHandler = () => {
+    setActiveNotification(null);
+  };
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        notification: activeNotification,
+        showNotification: showNotificationHandler,
+        hideNotification: hideNotificationHandler,
+      }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
 
 export default NotificationContext;
