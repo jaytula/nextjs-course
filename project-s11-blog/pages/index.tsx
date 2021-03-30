f79a3d1d18daf45ca1b9d977ee22ea4cc074ad62
Jay T <jaytula@gmail.com>
2021-03-30 09:48:53 -0700

    Lesson 11.175: Getting Started With The Home Page

--- project-s11-blog/pages/index.tsx
+++ project-s11-blog/pages/index.tsx
@@ -1,3 +1,13 @@
-const HomePage: React.FC = () => {}
+import { Fragment } from "react"
+
+const HomePage: React.FC = () => {
+  return <Fragment>
+    <Hero />
+    <FeaturedPosts />
+  </Fragment>
+}
 
 export default HomePage
+
+// 1) Hero => Present ourselves
+// 2) Featured Posts
