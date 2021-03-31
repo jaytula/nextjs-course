f318399d2fc3283ccfc3c2e5a3442a94df1a74f2
Jay T <jaytula@gmail.com>
2021-03-31 13:30:40 -0700

    Lesson 11.180: Adding A Post Grid & Post Items

--- project-s11-blog/components/posts/post-item.tsx
+++ project-s11-blog/components/posts/post-item.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { PostModel } from "../../models";
 import classes from "./post-item.module.css";
@@ -5,8 +6,37 @@ import classes from "./post-item.module.css";
 type PostItemProps = {
   post: PostModel;
 };
-const PostItem: React.FC<PostItemProps> = ({ post }) => {
-  return <div className={classes.post}></div>;
+
+const PostItem: React.FC<PostItemProps> = ({
+  post: { title, image, excerpt, date, slug },
+}) => {
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  return (
+    <li className={classes.post}>
+      <Link href={`/posts/${slug}`}>
+        <a>
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image}`}
+              alt={title}
+              width={300}
+              height={200}
+            />
+          </div>
+          <div>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 };
 
 export default PostItem;
--- project-s11-blog/models.ts
+++ project-s11-blog/models.ts
@@ -1,3 +1,7 @@
 export type PostModel = {
-
+  title: string;
+  image: string;
+  excerpt: string;
+  date: string;
+  slug: string;
 }
