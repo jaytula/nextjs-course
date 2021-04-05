c455cad25fc05bf68365351cdaefca3d798bdafe
Jay T <jaytula@gmail.com>
2021-04-05 13:27:35 -0700

    Lesson 11.189: Rendering Images With The "Next Image" Component (From Markdown)

--- project-s11-blog/components/posts/post-detail/post-content.tsx
+++ project-s11-blog/components/posts/post-detail/post-content.tsx
@@ -1,7 +1,8 @@
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
-import classes from './post-content.module.css';
-import { PostModel } from '../../../models';
+import classes from "./post-content.module.css";
+import { PostModel } from "../../../models";
+import Image from "next/image";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
@@ -11,14 +12,45 @@ const DUMMY_POST = {
   content: "# This is a first post ",
 };
 
-const PostContent: React.FC<{post: PostModel}> = ({post}) => {
+const PostContent: React.FC<{ post: PostModel }> = ({ post }) => {
   const { title, image, content, slug } = post;
 
   const imagePath = `/images/posts/${slug}/${image}`;
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 };
